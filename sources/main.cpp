3685cf00c9f97385cb6246b18eb5059191b69970
Marc-André Renaud <marenaud@cimeq.qc.ca>
2018-05-03 09:21:21 -0400

    Limit framerate.

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,12 +3,24 @@
 
 int main(int /*argc*/, char */*argv*/[]) {
   Game game;
-  game.init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720/2, 720, false); // NOLINT
+  game.init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720 / 2, 720, false); // NOLINT
 
-  while(game.running()) {
+  const int FPS = 60;
+  const int FRAME_DELAY = 1000 / FPS;
+  uint32_t frame_start;
+  uint32_t frame_time;
+
+  while (game.running()) {
+    frame_start = SDL_GetTicks();
+
     game.handle_events();
     game.update();
     game.render();
+
+    frame_time = SDL_GetTicks() - frame_start;
+
+    if (FRAME_DELAY > frame_time)
+      SDL_Delay(FRAME_DELAY - frame_time);
   }
 
   game.clean();
